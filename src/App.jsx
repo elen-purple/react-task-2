44fc9ce5862c54e8f6fc9b891beb27be2ff74eaf
elen-purple <anna.havrylova.2011@gmail.com>
2025-07-08 21:22:04 +0300

    try 1

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Greeting } from "./components/Greeting";
+import { Message } from "./components/Message";
+import { Button } from "./components/Buttton";
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting name="Anna" />
+      <Message text="Використайте компонент Message в компоненті App та передайте йому довільне повідомлення через атрибут text." />
+      <Button
+        onClick={() => {
+          console.log("Hello, world");
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
